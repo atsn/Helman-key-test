bc0c04f02656a07e11a16513548c2f7061b05cc6
Anders Stubberup <anders_stubberup@hotmail.com>
2017-12-18 11:35:24 +0100

    Have implementet helmanss key exchange

---
+++ Helman key test/Connection Maneger.cs
@@ -0,0 +1,194 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Numerics;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading;
+
+namespace Universal_TCP_Client
+{
+    internal class ConnectionHandler
+    {
+       
+        private static int index = 0;
+        private readonly NetworkStream clientStream;
+        private readonly StreamReader clientStreamReader;
+        private readonly StreamWriter clientStreamWriter;
+        public BigInteger n = BigInteger.Parse("9999999929");
+
+        public ConnectionHandler(string name, bool isclient, string ip = "")
+        {
+            if (isclient)
+            {
+                var client = new TcpClient(ip, 6789);
+                clientStream = client.GetStream();
+                Console.WriteLine("server forbundet");
+                clientStreamReader = new StreamReader(clientStream);
+                clientStreamWriter = new StreamWriter(clientStream);
+                clientStreamWriter.AutoFlush = true;
+                var g = BigInteger.Parse(clientStreamReader.ReadLine());
+                var serverkey = BigInteger.Parse(clientStreamReader.ReadLine());
+
+                MyKey = getrandombigint();
+                Thread.Sleep(1000);
+                var nexmessege = (Pow(g, MyKey) % n).ToString();
+                clientStreamWriter.WriteLineAsync(nexmessege);
+                SharedKey = (Pow(serverkey, MyKey) % n).ToString();
+                Console.WriteLine(SharedKey);
+            }
+            else
+            {
+                var server = new TcpListener(IPAddress.Any, 6789);
+                server.Start();
+                var serverclient = server.AcceptTcpClient();
+                clientStream = serverclient.GetStream();
+                Console.WriteLine("client forbundet");
+                clientStreamReader = new StreamReader(clientStream);
+                clientStreamWriter = new StreamWriter(clientStream);
+                clientStreamWriter.AutoFlush = true;
+
+                var g = getrandombigint();
+                MyKey = getrandombigint();
+                clientStreamWriter.WriteLineAsync(g.ToString());
+
+                var nexmessege = (Pow(g, MyKey) % n).ToString();
+                clientStreamWriter.WriteLineAsync(nexmessege);
+                Thread.Sleep(1000);
+
+                var clientkey = BigInteger.Parse(clientStreamReader.ReadLine());
+                SharedKey = (Pow(clientkey, MyKey) % n).ToString();
+                Console.WriteLine(SharedKey);
+            }
+
+
+            Name = name;
+        }
+
+        public string messege { get; set; } = "kør";
+        public string Name { get; set; }
+        public BigInteger MyKey { get; set; }
+        public string SharedKey { get; set; }
+
+
+        public void Reshive()
+        {
+            while (!messege.ToLower().Equals("stop"))
+                try
+                {
+                    var messege = clientStreamReader.ReadLine();
+                    Console.WriteLine(Decrypt(messege));
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e);
+                    break;
+                }
+        }
+
+        public void Sendmessege()
+        {
+            while (!messege.ToLower().Equals("stop"))
+                try
+                {
+                    clientStreamWriter.AutoFlush = true;
+
+                    messege = Name + ": " + Console.ReadLine();
+                    clientStreamWriter.WriteLine(Encrypt(messege));
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e);
+                    break;
+                }
+        }
+
+        public BigInteger getrandombigint()
+        {
+            BigInteger returnvalue = new BigInteger(10000000000000000000);
+
+            while (returnvalue > n)
+            {
+                var random = new Random();
+                var r = random.Next() % 10 + 1;
+                var randombig = "";
+                for (var i = 0; i < r; i++)
+                {
+                    randombig += random.Next() % 10;
+
+                }
+                returnvalue = BigInteger.Parse(randombig);
+            }
+        
+           
+
+            return returnvalue;
+        }
+
+        public BigInteger Pow(BigInteger value, BigInteger exponent)
+        {
+            var originalValue = value;
+            while (exponent-- > 1)
+                value = BigInteger.Multiply(value, originalValue);
+
+            return value;
+        }
+
+
+        public string Encrypt(string clearText)
+        {
+            var EncryptionKey = SharedKey;
+            var clearBytes = Encoding.Unicode.GetBytes(clearText);
+            using (var encryptor = Aes.Create())
+            {
+                var pdb = new Rfc2898DeriveBytes(EncryptionKey,
+                    new byte[]
+                    {
+                        0x49, 0x76, 0x61, 0x6e, 0x15, 0x20, 0x4d, 0x65, 0x61, 0x6e, 0x15, 0x20, 0x4d, 0x65, 0x64, 0x87,
+                        0x76, 0x65, 0x64, 0x65, 0x76
+                    });
+                encryptor.Key = pdb.GetBytes(32);
+                encryptor.IV = pdb.GetBytes(16);
+                using (var ms = new MemoryStream())
+                {
+                    using (var cs = new CryptoStream(ms, encryptor.CreateEncryptor(), CryptoStreamMode.Write))
+                    {
+                        cs.Write(clearBytes, 0, clearBytes.Length);
+                        cs.Close();
+                    }
+                    clearText = Convert.ToBase64String(ms.ToArray());
+                }
+            }
+            return clearText;
+        }
+
+        public string Decrypt(string cipherText)
+        {
+            var EncryptionKey = SharedKey;
+            cipherText = cipherText.Replace(" ", "+");
+            var cipherBytes = Convert.FromBase64String(cipherText);
+            using (var encryptor = Aes.Create())
+            {
+                var pdb = new Rfc2898DeriveBytes(EncryptionKey,
+                    new byte[]
+                    {
+                        0x49, 0x76, 0x61, 0x6e, 0x15, 0x20, 0x4d, 0x65, 0x61, 0x6e, 0x15, 0x20, 0x4d, 0x65, 0x64, 0x87,
+                        0x76, 0x65, 0x64, 0x65, 0x76
+                    });
+                encryptor.Key = pdb.GetBytes(32);
+                encryptor.IV = pdb.GetBytes(16);
+                using (var ms = new MemoryStream())
+                {
+                    using (var cs = new CryptoStream(ms, encryptor.CreateDecryptor(), CryptoStreamMode.Write))
+                    {
+                        cs.Write(cipherBytes, 0, cipherBytes.Length);
+                        cs.Close();
+                    }
+                    cipherText = Encoding.Unicode.GetString(ms.ToArray());
+                }
+            }
+            return cipherText;
+        }
+    }
+}
--- Helman key test/Program.cs
+++ Helman key test/Program.cs
@@ -1,8 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Net.Sockets;
 using System.Text;
 using System.Threading.Tasks;
+using System.Numerics;
+using System.Threading;
+using Universal_TCP_Client;
 
 namespace Helman_key_test
 {
@@ -10,6 +15,50 @@ namespace Helman_key_test
     {
         static void Main(string[] args)
         {
+ 
+                Console.WriteLine("Please enter name");
+                string name = Console.ReadLine();
+                Console.WriteLine("press 1 for client and 2 for server");
+                ConsoleKey pressedkey = Console.ReadKey(true).Key;
+
+                if (pressedkey.Equals(ConsoleKey.D1) || pressedkey.Equals(ConsoleKey.NumPad1))
+                {
+                    try
+                    {
+                        Console.WriteLine("please enter ip");
+                        string ip = Console.ReadLine();
+                        ConnectionHandler handler = new ConnectionHandler(name, true, ip);
+                        Thread Reshivethread = new Thread(handler.Reshive);
+                        Thread Sendthread = new Thread(handler.Sendmessege);
+                        Reshivethread.Start();
+                        Sendthread.Start();
+                        Console.WriteLine("Du kan nu skrive");
+
+                    }
+                    catch (Exception e)
+                    {
+
+                        Console.WriteLine(e);
+                    }
+                }
+
+                else if (pressedkey.Equals(ConsoleKey.D2) || pressedkey.Equals(ConsoleKey.NumPad2))
+                {
+
+
+
+                    ConnectionHandler handler = new ConnectionHandler(name, false);
+                    Thread Reshivethread = new Thread(handler.Reshive);
+                    Thread Sendthread = new Thread(handler.Sendmessege);
+                    Reshivethread.Start();
+                    Sendthread.Start();
+                    Console.WriteLine("Du kan nu skrive");
+                }
+                else
+                {
+                    Console.WriteLine("button not regconiced");
+                }
+
         }
     }
 }
